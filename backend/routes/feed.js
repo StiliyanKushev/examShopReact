67915322bf543f5b54b1d1f7d409c8e1d94bead1
Stiliyan Kushev <stiliyankushev@gmail.com>
2019-03-11 17:06:15 +0200

    moved the backend folder to the frontend folder so github can recognize it

--- backend/routes/feed.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const feedController = require('../controllers/feed');
-const isAuth = require('../middleware/is-auth');
-
-router.get('/products', feedController.getProducts);
-router.post('/product/create', feedController.createProduct);
-
-module.exports = router;
